992dcd95cade609c670a9a866560bb1cac33c2d5
Rafael Panisset <rafaelpani99@gmail.com>
2024-07-21 21:46:19 -0300

    alterada funcao main

--- app/Main.hs
+++ app/Main.hs
@@ -3,33 +3,41 @@ module Main where
 import Types
 import GameLogic
 import System.IO
-import Text.Read (readMaybe)
+import Control.Monad (when)
 
 main :: IO ()
-main = gameLoop initialGameState
-
-initialGameState :: GameState
-initialGameState = GameState AncientEgypt [] [] 0
+main = do
+    initialState <- addNewAnomaly $ GameState AncientEgypt [] [] [] 0
+    gameLoop initialState
 
 gameLoop :: GameState -> IO ()
 gameLoop state = do
-    putStrLn $ "Current Time: " ++ show (currentTime state)
+    putStrLn $ "\nCurrent Time: " ++ show (currentTime state)
     putStrLn $ "Inventory: " ++ show (map itemName $ inventory state)
     putStrLn $ "Time Jumps: " ++ show (timeJumps state)
-    putStrLn "Enter command (travel/pickup/use/quit):"
+    displayAnomalies state
+    putStrLn "\nEnter command (travel/pickup/use/quit):"
     command <- getLine
     case command of
         "travel" -> do
             putStrLn "Enter destination (AncientEgypt/Renaissance/FutureCity):"
             destinationStr <- getLine
             case reads destinationStr of
-                [(destination, "")] -> gameLoop $ timeTravel destination state
+                [(destination, "")] -> do
+                    let newState = timeTravel destination state
+                    updatedState <- addNewAnomaly newState
+                    gameLoop updatedState
                 _ -> putStrLn "Invalid destination" >> gameLoop state
         "pickup" -> do
             putStrLn "Enter item name:"
             itemName <- getLine
             let newItem = Item itemName "A mysterious item"
             gameLoop $ addItem newItem state
-        "use" -> putStrLn "Item usage not implemented yet" >> gameLoop state
+        "use" -> do
+            putStrLn "Enter item name to use:"
+            itemName <- getLine
+            let (newState, message) = useItem itemName state
+            putStrLn message
+            gameLoop newState
         "quit" -> putStrLn "Thanks for playing!"
         _ -> putStrLn "Unknown command" >> gameLoop state
